Fix CppGPs solver precision implementation

Comparison_with_GPyTorch.py
@@ -8,8 +8,14 @@ import os
 
 from Comparison_with_SciKit_Learn import getTrainingData, getSampleCount
 
+import matplotlib.pyplot as plt
+
 USE_Standard = True
 USE_SKI = True
+
+# Option to display training information for each iteration
+VERBOSE = False
+PLOT_LOSS = False
 
 # Evaluate GPyTorch Exact Gaussian Process Model on CppGPs training data
 def standard():
@@ -22,26 +28,31 @@ def standard():
     ##  REFERENCE PAPER: https://arxiv.org/pdf/1809.11165.pdf
     ##
 
-    # Specify number of training iterations and learning rate for optimizer
-    training_iter = 250
-    learning_rate = 0.1
-
-    # Option to display training information for each iteration
-    VERBOSE = False
-
-    if USE_SKI:
-        print("\n[ Standard Implementation ]")
 
     # Get training data from CppGPs setup
     inputDim, obsX, obsY, inVals = getTrainingData()
     if inputDim == 1:
         sampleCount = getSampleCount()
+    
+    # Specify number of training iterations and learning rate for optimizer
+    if inputDim == 1:
+        training_iter = 250
+    else:
+        training_iter = 75
+    learning_rate = 0.1
+
+    # Specify precision level for stopping criteria
+    eps = 2.220446049250313e-16
+    factr = 1e11
+    ftol = factr*eps
+
+    if USE_SKI:
+        print("\n[ Standard Implementation ]")
 
     # Convert to float32
     obsX = obsX.astype(np.float32)
     obsY = obsY.astype(np.float32)
     inVals = inVals.astype(np.float32)
-
 
     # Define exact inference Gaussian process regression model
     class GPRegressionModel(gpytorch.models.ExactGP):
@@ -101,24 +112,57 @@ def standard():
 
     # Define the training loop
     start_time = time.time()
+    f_prev = 1e9
+    loss_steps = []
+    loss_list = []
     for i in range(training_iter):
+
+        #learning_decay_step = 5
+        #if np.mod(i,learning_decay_step) == 0:
+        #    learning_decay = 0.9
+        #    learning_rate = learning_rate*learning_decay
+        #    for param_group in optimizer.param_groups:
+        #        param_group['lr'] = learning_rate
+
+        #for param_group in optimizer.param_groups:
+        #    print("{:.4e}".format(param_group['lr']))
+        
+        
         optimizer.zero_grad()
         output = model(train_x)
         # Calculate loss and backprop gradients
         loss = -mll(output, train_y)
-        loss.backward()    
+        loss.backward()
         if VERBOSE:
-            print('Iter %d/%d - Loss: %.3f   lengthscale: %.3f   noise: %.3f' % (
+            print('Iter %d/%d - Loss: %.3f   outputscale: %.3f lengthscale: %.3f   noise: %.3f' % (
                 i + 1, training_iter, loss.item()*obsCount,
+                np.sqrt(model.covar_module.outputscale.item()),
                 model.covar_module.base_kernel.lengthscale.item(),
                 model.likelihood.noise.item()
             ))
+
+        ### Try enforcing stopping criteria; NLML values seem to be too noisy...
+        """
+        f_current = loss.item()*obsCount
+        stopping_crit = np.abs((f_current - f_prev)/(f_current))
+        if VERBOSE:
+            print("[ Stopping Criteria: {:.4e} < {:.4e} ]   Moving Average = {:.4f} ".format(stopping_crit,ftol,f_prev))
+        if stopping_crit < ftol:
+            break;
+        # Define exponential moving average to account for noise
+        alpha = 0.25
+        f_prev = alpha*f_current + (1-alpha)*f_prev
+        """
+        if PLOT_LOSS:
+            loss_steps.append(i)
+            loss_list.append(loss.item()*obsCount)
         optimizer.step()
-
-
 
     end_time = time.time()
 
+    if VERBOSE:
+        print("\n[*] Function Evaluations: {}".format(i+1))
+    
     # Display computation time 
     print('\nComputation Time:  {:.5f} s \n'.format(end_time-start_time))
 
@@ -171,8 +215,13 @@ def standard():
         samples = posterior_distribution.rsample(sample_shape=torch.Size([sampleCount]))
 
 
+    # Plot loss values from training procedure
+    if PLOT_LOSS:
+        plt.plot(np.array(loss_steps),np.array(loss_list))
+        plt.show()
 
-    GPyTorch_results_dir = "./GPyTorch_Results/"
+
+    GPyTorch_results_dir = "./gpytorch_results/"
 
     if not os.path.exists(GPyTorch_results_dir):
         os.makedirs(GPyTorch_results_dir)
@@ -212,24 +261,26 @@ def SKI():
     ##  REFERENCE PAPER: https://arxiv.org/pdf/1809.11165.pdf
     ##
 
-    # Specify number of training iterations and learning rate for optimizer
-    training_iter = 250
-    learning_rate = 0.1
-
-    # Option to display training information for each iteration
-    VERBOSE = False
-
-    if USE_Standard:
-        print("\n[ SKI Implementation ]")
-
     # Get training data from CppGPs setup
     inputDim, obsX, obsY, inVals = getTrainingData()
     if inputDim == 1:
         sampleCount = getSampleCount()
 
-    if ( not inputDim == 1 ):
-        print("\n[*] The SKI model is currently only implemented for one dimensional problems\n")
-        return -1
+    # Specify number of training iterations and learning rate for optimizer
+    if inputDim == 1:
+        training_iter = 250
+    else:
+        training_iter = 75
+    learning_rate = 0.1
+
+    # Specify precision level for stopping criteria
+    eps = 2.220446049250313e-16
+    factr = 1e10
+    ftol = factr*eps
+
+
+    if USE_Standard:
+        print("\n[ SKI Implementation ]")
 
     # Convert to float32
     obsX = obsX.astype(np.float32)
@@ -240,13 +291,12 @@ def SKI():
     class GPRegressionModel(gpytorch.models.ExactGP):
         def __init__(self, train_x, train_y, likelihood):
             super(GPRegressionModel, self).__init__(train_x, train_y, likelihood)
-            # SKI requires a grid size hyperparameter. This util can help with that. Here we are using a grid that has the same number of points as the training data (a ratio of 1.0). Performance can be sensitive to this parameter, so you may want to adjust it for your own problem on a validation set.
             grid_size = gpytorch.utils.grid.choose_grid_size(train_x,1.0)
 
             self.mean_module = gpytorch.means.ConstantMean()
             self.covar_module = gpytorch.kernels.GridInterpolationKernel(
                 gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel()),
-                grid_size=grid_size, num_dims=1,
+                grid_size=grid_size, num_dims=inputDim,
             )
 
         def forward(self, x):
@@ -279,7 +329,7 @@ def SKI():
     uniform_dist = torch.distributions.Uniform(-1.0, 1.0)
     #train_x = uniform_dist.rsample(sample_shape=torch.Size([obsCount, inputDim]))
     train_x = torch.from_numpy(obsX)
-
+    
     # Generate noisy observation target data
     #train_y = applyFunc(targetFunc, train_x)
     #train_y = train_y + noise
@@ -301,24 +351,53 @@ def SKI():
 
     # Define the training loop
     start_time = time.time()
+    f_prev = 1e9
     for i in range(training_iter):
+
+        # Try using learning rate decay
+        """
+        learning_decay_step = 5
+        if np.mod(i,learning_decay_step) == 0:
+            learning_decay = 0.9
+            learning_rate = learning_rate*learning_decay
+            for param_group in optimizer.param_groups:
+                param_group['lr'] = learning_rate
+
+        #for param_group in optimizer.param_groups:
+        #    print("{:.4e}".format(param_group['lr']))
+        """
+        
         optimizer.zero_grad()
         output = model(train_x)
         # Calculate loss and backprop gradients
         loss = -mll(output, train_y)
         loss.backward()    
-        #if VERBOSE:
-        #    print('Iter %d/%d - Loss: %.3f   lengthscale: %.3f   noise: %.3f' % (
-        #        i + 1, training_iter, loss.item()*obsCount,
-        #        model.covar_module.base_kernel.lengthscale.item(),
-        #        model.likelihood.noise.item()
-        #    ))
+        if VERBOSE:
+            print('Iter %d/%d - Loss: %.3f   outputscale: %.3f lengthscale: %.3f   noise: %.3f' % (
+                i + 1, training_iter, loss.item()*obsCount,
+                np.sqrt(model.covar_module.base_kernel.outputscale.item()),
+                model.covar_module.base_kernel.base_kernel.lengthscale.item(),
+                model.likelihood.noise.item()
+            ))
+        ### Try enforcing stopping criteria; NLML values seem to be too noisy...
+        """
+        f_current = loss.item()*obsCount
+        stopping_crit = np.abs((f_current - f_prev)/(f_current))
+        if VERBOSE:
+            print("[ Stopping Criteria: {:.4e} < {:.4e} ] ".format(stopping_crit,ftol))
+        if stopping_crit < ftol:
+            break;
+        f_prev = f_current
+        """
         optimizer.step()
 
 
 
     end_time = time.time()
 
+    if VERBOSE:
+        print("\n[*] Function Evaluations: {}".format(i+1))
+    
     # Display computation time 
     print('\nComputation Time:  {:.5f} s \n'.format(end_time-start_time))
 
@@ -341,7 +420,7 @@ def SKI():
 
     # Unwrap the GridInterpolationKernel to obtain underlying ScaleKernel
     scale_kernel = model.covar_module.base_kernel
-    
+
     print("Optimized Hyperparameters:")
     kernel_outputscale = scale_kernel.outputscale.item()
     length_scale = scale_kernel.base_kernel.lengthscale.item()
@@ -361,14 +440,15 @@ def SKI():
     # Define true solution values on test mesh
     #trueSoln = applyFunc(targetFunc, testMesh)
 
-    # Get posterior distributions for the mean and output values
-    mean_predictive_distribution = model(testMesh)
-    posterior_distribution = likelihood(model(testMesh))
-
-    # Get posterior means and variances
-    pred_mean = posterior_distribution.mean
-    pred_var = posterior_distribution.variance
-    pred_covar = posterior_distribution.covariance_matrix
+    with torch.no_grad(), gpytorch.settings.fast_pred_var():    
+        # Get posterior distributions for the mean and output values
+        mean_predictive_distribution = model(testMesh)
+        posterior_distribution = likelihood(model(testMesh))
+        
+        # Get posterior means and variances
+        pred_mean = posterior_distribution.mean
+        pred_var = posterior_distribution.variance
+        pred_covar = posterior_distribution.covariance_matrix
 
     if inputDim == 1:
         # Get sample paths from posterior
@@ -376,7 +456,7 @@ def SKI():
 
 
 
-    GPyTorch_results_dir = "./GPyTorch_Results/"
+    GPyTorch_results_dir = "./gpytorch_results/"
 
     if not os.path.exists(GPyTorch_results_dir):
         os.makedirs(GPyTorch_results_dir)

tests/Comparison_with_GPyTorch.py
@@ -8,9 +8,17 @@ import os
 
 from Comparison_with_SciKit_Learn import getTrainingData, getSampleCount
 
+import matplotlib.pyplot as plt
+
+USE_Standard = True
+USE_SKI = True
+
+# Option to display training information for each iteration
+VERBOSE = False
+PLOT_LOSS = False
 
 # Evaluate GPyTorch Exact Gaussian Process Model on CppGPs training data
-def main():
+def standard():
 
     dtype = torch.float
     device = torch.device("cpu")
@@ -20,29 +28,36 @@ def main():
     ##  REFERENCE PAPER: https://arxiv.org/pdf/1809.11165.pdf
     ##
 
-    # Specify number of training iterations and learning rate for optimizer
-    training_iter = 250
-    learning_rate = 0.1
-
-    # Option to display training information for each iteration
-    VERBOSE = False
-
 
     # Get training data from CppGPs setup
     inputDim, obsX, obsY, inVals = getTrainingData()
     if inputDim == 1:
         sampleCount = getSampleCount()
+    
+    # Specify number of training iterations and learning rate for optimizer
+    if inputDim == 1:
+        training_iter = 250
+    else:
+        training_iter = 75
+    learning_rate = 0.1
+
+    # Specify precision level for stopping criteria
+    eps = 2.220446049250313e-16
+    factr = 1e11
+    ftol = factr*eps
+
+    if USE_SKI:
+        print("\n[ Standard Implementation ]")
 
     # Convert to float32
     obsX = obsX.astype(np.float32)
     obsY = obsY.astype(np.float32)
     inVals = inVals.astype(np.float32)
 
-
     # Define exact inference Gaussian process regression model
-    class ExactGPModel(gpytorch.models.ExactGP):
+    class GPRegressionModel(gpytorch.models.ExactGP):
         def __init__(self, train_x, train_y, likelihood):
-            super(ExactGPModel, self).__init__(train_x, train_y, likelihood)
+            super(GPRegressionModel, self).__init__(train_x, train_y, likelihood)
             self.mean_module = gpytorch.means.ConstantMean()
             self.covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel())
 
@@ -83,7 +98,7 @@ def main():
 
     # Initialize the likelihood function and GP model
     likelihood = gpytorch.likelihoods.GaussianLikelihood()
-    model = ExactGPModel(train_x, train_y, likelihood)    
+    model = GPRegressionModel(train_x, train_y, likelihood)    
 
     # Find optimal model hyperparameters
     model.train()
@@ -97,24 +112,57 @@ def main():
 
     # Define the training loop
     start_time = time.time()
+    f_prev = 1e9
+    loss_steps = []
+    loss_list = []
     for i in range(training_iter):
+
+        #learning_decay_step = 5
+        #if np.mod(i,learning_decay_step) == 0:
+        #    learning_decay = 0.9
+        #    learning_rate = learning_rate*learning_decay
+        #    for param_group in optimizer.param_groups:
+        #        param_group['lr'] = learning_rate
+
+        #for param_group in optimizer.param_groups:
+        #    print("{:.4e}".format(param_group['lr']))
+        
+        
         optimizer.zero_grad()
         output = model(train_x)
         # Calculate loss and backprop gradients
         loss = -mll(output, train_y)
-        loss.backward()    
+        loss.backward()
         if VERBOSE:
-            print('Iter %d/%d - Loss: %.3f   lengthscale: %.3f   noise: %.3f' % (
+            print('Iter %d/%d - Loss: %.3f   outputscale: %.3f lengthscale: %.3f   noise: %.3f' % (
                 i + 1, training_iter, loss.item()*obsCount,
+                np.sqrt(model.covar_module.outputscale.item()),
                 model.covar_module.base_kernel.lengthscale.item(),
                 model.likelihood.noise.item()
             ))
+
+        ### Try enforcing stopping criteria; NLML values seem to be too noisy...
+        """
+        f_current = loss.item()*obsCount
+        stopping_crit = np.abs((f_current - f_prev)/(f_current))
+        if VERBOSE:
+            print("[ Stopping Criteria: {:.4e} < {:.4e} ]   Moving Average = {:.4f} ".format(stopping_crit,ftol,f_prev))
+        if stopping_crit < ftol:
+            break;
+        # Define exponential moving average to account for noise
+        alpha = 0.25
+        f_prev = alpha*f_current + (1-alpha)*f_prev
+        """
+        if PLOT_LOSS:
+            loss_steps.append(i)
+            loss_list.append(loss.item()*obsCount)
         optimizer.step()
-
-
 
     end_time = time.time()
 
+    if VERBOSE:
+        print("\n[*] Function Evaluations: {}".format(i+1))
+    
     # Display computation time 
     print('\nComputation Time:  {:.5f} s \n'.format(end_time-start_time))
 
@@ -167,8 +215,13 @@ def main():
         samples = posterior_distribution.rsample(sample_shape=torch.Size([sampleCount]))
 
 
+    # Plot loss values from training procedure
+    if PLOT_LOSS:
+        plt.plot(np.array(loss_steps),np.array(loss_list))
+        plt.show()
 
-    GPyTorch_results_dir = "./GPyTorch_Results/"
+
+    GPyTorch_results_dir = "./gpytorch_results/"
 
     if not os.path.exists(GPyTorch_results_dir):
         os.makedirs(GPyTorch_results_dir)
@@ -195,9 +248,247 @@ def main():
         # Save NLML
         filename = os.path.join(GPyTorch_results_dir, "NLML.npy")
         np.save(filename, NLML)
+
+
+# Evaluate GPyTorch Exact Gaussian Process Model on CppGPs training data
+def SKI():
+
+    dtype = torch.float
+    device = torch.device("cpu")
+    # device = torch.device("cuda:0") # Uncomment this to run on GPU
+
+    ##
+    ##  REFERENCE PAPER: https://arxiv.org/pdf/1809.11165.pdf
+    ##
+
+    # Get training data from CppGPs setup
+    inputDim, obsX, obsY, inVals = getTrainingData()
+    if inputDim == 1:
+        sampleCount = getSampleCount()
+
+    # Specify number of training iterations and learning rate for optimizer
+    if inputDim == 1:
+        training_iter = 250
+    else:
+        training_iter = 75
+    learning_rate = 0.1
+
+    # Specify precision level for stopping criteria
+    eps = 2.220446049250313e-16
+    factr = 1e10
+    ftol = factr*eps
+
+
+    if USE_Standard:
+        print("\n[ SKI Implementation ]")
+
+    # Convert to float32
+    obsX = obsX.astype(np.float32)
+    obsY = obsY.astype(np.float32)
+    inVals = inVals.astype(np.float32)
+
+
+    class GPRegressionModel(gpytorch.models.ExactGP):
+        def __init__(self, train_x, train_y, likelihood):
+            super(GPRegressionModel, self).__init__(train_x, train_y, likelihood)
+            grid_size = gpytorch.utils.grid.choose_grid_size(train_x,1.0)
+
+            self.mean_module = gpytorch.means.ConstantMean()
+            self.covar_module = gpytorch.kernels.GridInterpolationKernel(
+                gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel()),
+                grid_size=grid_size, num_dims=inputDim,
+            )
+
+        def forward(self, x):
+            mean_x = self.mean_module(x)
+            covar_x = self.covar_module(x)
+            return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
+
+
+    # Define the target function to be an oscillatory, non-periodic function
+    def targetFunc(X):
+        oscillation = 30.0
+        xshifted = 0.5*(X[0] + 1.0)
+        return np.sin(oscillation*(xshifted-0.1))*(0.5-(xshifted-0.1))*15.0
+
+    # Define utility function for applying Python function to PyTorch tensor
+    def applyFunc(func, Tensor):
+        tList = [func(m) for m in torch.unbind(Tensor, dim=0) ]
+        result = torch.stack(tList, dim=0)
+        return result
+
+    # Specify observation count and input dimension
+    obsCount = obsX.shape[0]
+
+    # Construct noise for observation data
+    noiseLevel = 1.0
+    normal_dist =  torch.distributions.Normal(0.0,noiseLevel)
+    noise = normal_dist.rsample(sample_shape=torch.Size([obsCount]))
+
+    # Generate random input observation data
+    uniform_dist = torch.distributions.Uniform(-1.0, 1.0)
+    #train_x = uniform_dist.rsample(sample_shape=torch.Size([obsCount, inputDim]))
+    train_x = torch.from_numpy(obsX)
     
+    # Generate noisy observation target data
+    #train_y = applyFunc(targetFunc, train_x)
+    #train_y = train_y + noise
+    train_y = torch.from_numpy(obsY)
+
+    # Initialize the likelihood function and GP model
+    likelihood = gpytorch.likelihoods.GaussianLikelihood()
+    model = GPRegressionModel(train_x, train_y, likelihood)    
+
+    # Find optimal model hyperparameters
+    model.train()
+    likelihood.train()
+
+    # Use the Adam optimizer to tune hyperparameters
+    optimizer = torch.optim.Adam([ {'params': model.parameters()}, ], lr=learning_rate)
+
+    # Define (negative) loss function for GP model to be the marginal log likelihood
+    mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
+
+    # Define the training loop
+    start_time = time.time()
+    f_prev = 1e9
+    for i in range(training_iter):
+
+        # Try using learning rate decay
+        """
+        learning_decay_step = 5
+        if np.mod(i,learning_decay_step) == 0:
+            learning_decay = 0.9
+            learning_rate = learning_rate*learning_decay
+            for param_group in optimizer.param_groups:
+                param_group['lr'] = learning_rate
+
+        #for param_group in optimizer.param_groups:
+        #    print("{:.4e}".format(param_group['lr']))
+        """
+        
+        optimizer.zero_grad()
+        output = model(train_x)
+        # Calculate loss and backprop gradients
+        loss = -mll(output, train_y)
+        loss.backward()    
+        if VERBOSE:
+            print('Iter %d/%d - Loss: %.3f   outputscale: %.3f lengthscale: %.3f   noise: %.3f' % (
+                i + 1, training_iter, loss.item()*obsCount,
+                np.sqrt(model.covar_module.base_kernel.outputscale.item()),
+                model.covar_module.base_kernel.base_kernel.lengthscale.item(),
+                model.likelihood.noise.item()
+            ))
+        ### Try enforcing stopping criteria; NLML values seem to be too noisy...
+        """
+        f_current = loss.item()*obsCount
+        stopping_crit = np.abs((f_current - f_prev)/(f_current))
+        if VERBOSE:
+            print("[ Stopping Criteria: {:.4e} < {:.4e} ] ".format(stopping_crit,ftol))
+        if stopping_crit < ftol:
+            break;
+        f_prev = f_current
+        """
+        optimizer.step()
+
+
+
+    end_time = time.time()
+
+    if VERBOSE:
+        print("\n[*] Function Evaluations: {}".format(i+1))
+    
+    # Display computation time 
+    print('\nComputation Time:  {:.5f} s \n'.format(end_time-start_time))
+
+
+    ##
+    #   [ Posterior Predictions and Samples ]
+    ##
+
+    with torch.no_grad():
+        # They appear to scale the NLML calculation by 1/obsCount
+        # (also this must be run before issuing the ".eval()" statements)
+        NLML = -mll.forward(model(train_x), train_y)
+        NLML = obsCount * NLML 
+
+
+    # Get into evaluation (predictive posterior) mode
+    model.eval()
+    likelihood.eval()
+
+
+    # Unwrap the GridInterpolationKernel to obtain underlying ScaleKernel
+    scale_kernel = model.covar_module.base_kernel
+
+    print("Optimized Hyperparameters:")
+    kernel_outputscale = scale_kernel.outputscale.item()
+    length_scale = scale_kernel.base_kernel.lengthscale.item()
+    predictive_noise = model.likelihood.noise.item()
+    print(" {:.4f}**2 * {:.4f}   (Noise = {:.4f})\n".format(np.sqrt(kernel_outputscale), length_scale, predictive_noise))
+
+    print("NLML = {:.4f}\n".format(NLML))
+
+
+    # Specify the input mesh for testing
+    #predCount = inVals.shape[0]
+    #testMesh = torch.linspace(-1.0, 1.0, steps=predCount)
+    #testMesh = testMesh.unsqueeze(0)
+    #testMesh = torch.transpose(testMesh, 0, 1)
+    testMesh = torch.from_numpy(inVals)
+
+    # Define true solution values on test mesh
+    #trueSoln = applyFunc(targetFunc, testMesh)
+
+    with torch.no_grad(), gpytorch.settings.fast_pred_var():    
+        # Get posterior distributions for the mean and output values
+        mean_predictive_distribution = model(testMesh)
+        posterior_distribution = likelihood(model(testMesh))
+        
+        # Get posterior means and variances
+        pred_mean = posterior_distribution.mean
+        pred_var = posterior_distribution.variance
+        pred_covar = posterior_distribution.covariance_matrix
+
+    if inputDim == 1:
+        # Get sample paths from posterior
+        samples = posterior_distribution.rsample(sample_shape=torch.Size([sampleCount]))
+
+
+
+    GPyTorch_results_dir = "./gpytorch_results/"
+
+    if not os.path.exists(GPyTorch_results_dir):
+        os.makedirs(GPyTorch_results_dir)
+
+    # Save results to file
+    with torch.no_grad():
+
+        # Save prediction data
+        filename = os.path.join(GPyTorch_results_dir, "SKI_predMean.npy")
+        predMean = pred_mean.detach().numpy()
+        np.save(filename, predMean)
+
+        filename = os.path.join(GPyTorch_results_dir, "SKI_predStd.npy")
+        predStd = np.sqrt(pred_var.detach().numpy())
+        np.save(filename, predStd)
+
+
+        if inputDim == 1:
+            # Save posterior samples
+            filename = os.path.join(GPyTorch_results_dir, "SKI_samples.npy")
+            samples = samples.detach().numpy()
+            np.save(filename, samples)
+
+        # Save NLML
+        filename = os.path.join(GPyTorch_results_dir, "SKI_NLML.npy")
+        np.save(filename, NLML)
+        
 
 # Run main() function when called directly
 if __name__ == '__main__':
-    main()
+    if USE_Standard:
+        standard()
+    if USE_SKI:
+        SKI()
     

tests/Comparison_with_SciKit_Learn.py
@@ -6,6 +6,8 @@ import os
 import csv
 import time
 
+# Option to verify the CppGPs NLML calculation using the SciKit Learn framework
+VERIFY_NLML = False
 
 # Evaluate SciKit Learn Gaussian Process Regressor on CppGPs training data
 def main():
@@ -53,9 +55,26 @@ def main():
     NLML = -model.log_marginal_likelihood()
     print("NLML:  {:.4f}\n".format(NLML))
 
+
+
+    # Test the NLML calculation from the CppGPs optimal parameter results    
+    if VERIFY_NLML:
+        filename = "params.csv"
+        params = []
+        with open(filename, "r") as csvfile:
+            csvreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            for row in csvreader:
+                p = row
+        params.append(np.array(p))
+        params = np.array(params[0]).astype(np.float64)
+        theta = np.log(params)
+        CppGPs_NLML = -model.log_marginal_likelihood(theta = theta)
+        print("CppGPs NLML = {:.4f}\n".format(CppGPs_NLML))
+    
+
     
     # Save results to file
-    SciKit_Learn_results_dir = "./SciKit_Learn_Results/"
+    SciKit_Learn_results_dir = "./scikit_learn_results/"
 
     if not os.path.exists(SciKit_Learn_results_dir):
         os.makedirs(SciKit_Learn_results_dir)